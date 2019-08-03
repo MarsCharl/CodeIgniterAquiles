f8a3aaad2b212d3780536eaa96bfa4fa0944c189
CC102@ccs.local <CC102@ccs.local>
2019-08-04 04:21:23 +0800

    News Section

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,17 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 //
-$route['default_controller'] = 'pages/view';
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
-//$route['default_controller'] = 'welcome';
-//$route['404_override'] = '';
-//$route['translate_uri_dashes'] = FALSE;
-//$route['news/(:any)'] = 'news/view/$1';
-//$route['news'] = 'news';
-//$route['(:any)'] = 'pages/view/$1';
-//$route['default_controller'] = 'pages/view';
-//$route['news/create'] = 'news/create';
-//$route['news/(:any)'] = 'news/view/$1';
-//$route['news'] = 'news';
-//$route['(:any)'] = 'pages/view/$1';
-//$route['default_controller'] = 'pages/view';
+$route['default_controller'] = 'pages/view';
